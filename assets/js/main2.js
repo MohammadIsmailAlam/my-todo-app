e4d7978fb13831a7be0b327cde72ddbf481c0955
mohammadIsamil <ismailalam901@gmail.comm>
2022-12-08 15:20:08 +0600

    My todo list

---
+++ assets/js/main2.js
@@ -0,0 +1,16 @@
+(
+    function($) {
+        "use strict";
+    
+        jQuery(document).ready(function($){
+            $(".video-play-btn").magnificPopUp ({
+                type: 'video',
+            });
+
+        });
+
+        jQuery(window).load(function(){
+    
+        });
+    }(jQuery)
+    );
